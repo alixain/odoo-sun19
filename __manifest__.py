6db5b46157e9c5cd7c464ddcc55b7828ec37ef9c
Ali Zain <pcgcheats@gmail.com>
2026-01-28 17:32:56 +0500

    initial commit

--- __manifest__.py
+++ __manifest__.py
@@ -12,9 +12,9 @@
     'depends': ['sale', 'account', 'hr_payroll_community'],
     'data': [
         'views/sale_order_views.xml',
-        'views/account_move_views.xml',
-        'views/hr_payslip_views.xml',
-        'reports/invoice_report_templates.xml',
+        # 'views/account_move_views.xml',
+        # 'views/hr_payslip_views.xml',
+        # 'reports/invoice_report_templates.xml',
     ],
     'installable': True,
     'application': True,
